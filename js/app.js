8b103b7e63e0535ff8fb34edb09a3e131ed02e57
Maddie <bernkaztel@hotmail.com>
2018-02-25 17:55:09 -0600

    subiendo algoritmo de luhn y estilos

--- js/app.js
+++ js/app.js
@@ -2,38 +2,44 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    if (validateCardDetails(form)) {
-        console.log("datos válido... enviar...");
-    } else {
+    if (validateCardDetails(form) === true) {
+        console.log("datos válidos");
+    } else if (validateCardDetails(form) === false){
         console.log("datos inválidos");
     }
 });
 
+//Funcion para validar tarjeta
 function validateCardDetails(element) {
     const cardNumber = (element.cn.value);
     const expiryDate = (element.exp.value);
     const cardVerificationValue = (element.cvv.value);
     const cardName = (element.name.value);
-    cardTests(cardNumber, expiryDate, cardVerificationValue, cardName);
-}
-
-
-
-const cardTests = (cardNumber, expiryDate, cardVerificationValue, cardName) => {
     if (checkDate(expiryDate) === false) {
-        alert('Fecha no valida');
-    } else if (checkCVV(cardVerificationValue) === false) {
-        alert('CVV no valido');
+        console.log('Fecha no valida');
+        element.exp.classList.add("error");
+    }else if (checkCVV(cardVerificationValue) === false) {
+        console.log('CVV no valido');
+        element.cvv.classList.add("error");
     } else if (checkName(cardName) === false) {
-        alert('Nombre no valido');
+        console.log('Nombre no valido');
+        element.name.classList.add("error");
+    } else if (checkCardNumber(cardNumber) === false) {
+        console.log('Formato de numero de tarjeta no valido');
+        element.cn.classList.add("error");
+    } else if (checkLuhnAlgorithm(cardNumber) === true) {
+            console.log('Numero de tarjeta valido segun algoritmo');
+            element.cn.classList.add("success");
+            return true;
+        }
+        else {
+            element.cn.classList.add("error");
+            return false;
+        }
     }
-    else if (checkCardNumber(cardNumber) === false) {
-        alert('Numero de tarjeta no valido');
-    }
-    else if (checkCardNumber(cardNumber) === true) {
-       checkLuhnAlgorithm();
-    }
-};
+
+
+
 
 //Revisar fecha: string vacio, solo numeros, formato, formato mes, formato ano. 
 const checkDate = expiryDate => {
@@ -126,7 +132,114 @@ const checkCardNumber = cardNumber => {
         console.log('Contiene caracteres no validos');
         return false;
     } else {
-        console.log('El numero de tarjeta es correcto');
+        console.log('Pasa las pruebas de formato de tarjeta');
         return true;
+    }
+};
+
+
+
+
+//Revisa numero de tarjeta con algoritmo de luhn
+const checkLuhnAlgorithm = cardNumber => {
+    const cardNumbersArray = Array.from(cardNumber);
+    //Numeros son colocados al reves
+    const reversedCardNumbers = cardNumbersArray.reverse();
+    const evenReversedCardNumbers = [];
+    const oddReversedCardNumbers = [];
+
+    //Array de numeros en posicion par
+    for (let [index, value] of reversedCardNumbers.entries()) {
+        if (index % 2 == 0) {
+            evenReversedCardNumbers.push(value);
+        }
+    }
+    //Array de numeros en posicion impar
+    for (let [index, value] of reversedCardNumbers.entries()) {
+        if (index % 2 !== 0) {
+            oddReversedCardNumbers.push(value);
+        }
+    }
+    // console.log(evenReversedCardNumbers);
+    // console.log(oddReversedCardNumbers);
+
+    //Multiplicacion de numeros en posicion impar
+    let OddMultipledNumbers = oddReversedCardNumbers.map(function (number) {
+        return number * 2;
+    });
+    // console.log(OddMultipledNumbers);
+
+    //Array de numeros con mas de dos digitos
+    let twoDigitsOddMultipledNumbers = OddMultipledNumbers.filter(function (
+        number
+    ) {
+        return number > 9;
+    });
+    // console.log(twoDigitsOddMultipledNumbers);
+
+    //Array de numeros de un digito
+    let oneDigitOddMultipledNumbers = OddMultipledNumbers.filter(function (
+        number
+    ) {
+        return number <= 9;
+    });
+    // console.log(oneDigitOddMultipledNumbers);
+    var sumTwoDigits = [];
+
+    //Suma de numeros de dos digitos
+    twoDigitsOddMultipledNumbers.forEach(function (element) {
+        let element2 = element.toString();
+        let firstDigit = parseInt(element2[0]);
+        let secondDigit = parseInt(element2[1]);
+        sumTwoDigits.push(firstDigit + secondDigit);
+    });
+
+    // console.log(sumTwoDigits);
+
+    //Suma de todos los numeros de dos digitos
+    let totalTwoDigitSum = sumTwoDigits.reduce(function (
+        valorAnterior,
+        valorActual,
+        indice,
+        vector
+    ) {
+        return valorAnterior + valorActual;
+    });
+
+    //Suma de todos los numeros de un digitos
+    let totalOneDigitSum = oneDigitOddMultipledNumbers.reduce(function (
+        valorAnterior,
+        valorActual,
+        indice,
+        vector
+    ) {
+        return valorAnterior + valorActual;
+    });
+    //Suma de todos los numeros impares
+    let totalsumOdd = totalOneDigitSum + totalTwoDigitSum;
+    // console.log(totalOneDigitSum);
+    // console.log(totalTwoDigitSum);
+    // console.log(totalsumOdd);
+
+    //Suma de todos los numeros pares
+    let totalSumEven = evenReversedCardNumbers.reduce(function (
+        valorAnterior,
+        valorActual,
+        indice,
+        vector
+    ) {
+        return parseInt(valorAnterior) + parseInt(valorActual);
+    });
+    // console.log(totalSumEven);
+
+
+    //Suma de total de numeros pares e  impares
+    let totalSum = totalSumEven + totalsumOdd;
+    //Condicion si es divisible entre 10 
+    if (totalSum % 10 === 0) {
+        console.log('El numero de tarjeta es valido');
+        return true;
+    } else {
+        return false;
     }
 };
